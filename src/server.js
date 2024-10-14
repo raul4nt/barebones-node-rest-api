ee5068f019b4428b2853eb2c0eea5fe05cdac33e
Raul Silveira <raulantsilveira@gmail.com>
2024-10-14 19:11:18 -0300

    using status code with writeHead

--- src/server.js
+++ src/server.js
@@ -21,9 +21,33 @@ import http from 'node:http'
 // GET /users => Buscando usuários no banc-end
 // POST /users => Criar um usuário no back-end
 
+//Stateful - Stateless
+
+//Cabeçalhos (Requisição/resposta) => Metadados
+
+
+const users = []
 
 const server = http.createServer((req, res) => {
-    return res.end('Hello Ignite')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end('Not found')
 })
 
 server.listen(3333)
