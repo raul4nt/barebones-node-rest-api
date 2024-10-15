48bed6f3a119a19b28f28e8a2e2dda3ad0900639
Raul Silveira <raulantsilveira@gmail.com>
2024-10-15 00:12:52 -0300

    using randomUUID to generate a unique id for each user

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { Database } from './database.js'
+import { randomUUID } from 'node:crypto'
 // CommonJS => Require 
 // ESModules => import/export
 
@@ -51,7 +52,7 @@ const server = http.createServer(async (req, res) => {
     const { name, email } = req.body
     
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     }
