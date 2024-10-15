d4a44ef29534fd87665d8b49a8ae507ddd17c8e2
Raul Silveira <raulantsilveira@gmail.com>
2024-10-15 00:10:04 -0300

    using node:fs to persist the data in a json db

--- src/database.js
+++ src/database.js
@@ -1,8 +1,36 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+// usamos essa classe URL nativa do js para determinar
+// o caminho que é para o writeFile criar o db.json pois
+// se nao ele sempre criara com base na pasta que estamos 
+// aberto no terminal. ex: se eu nao tenho o db.json criado,
+// e no terminal to na pasta streams, o db.json sera criado la,
+// e isso nao é pra acontecer, quero que seja sempre na raiz do meu
+// projeto
+
 export class Database {
     #database = {}
     // esse # faz com que database seja uma prop privada,
     // entao nao conseguimos acessar ele diretamente
     // isso é otimo quando se trata de db
+
+
+    constructor () {
+        fs.readFile(databasePath, 'utf8')
+        .then(data =>{
+            this.#database = JSON.parse(data)
+        })
+        .catch(() => {
+            this.#persist()
+        })
+    }
+
+
+
+    #persist () {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -11,11 +39,13 @@ export class Database {
     }
 
     insert(table, data) {
-        if (Array.isArray(this.database[table])) {
+        if (Array.isArray(this.#database[table])) {
             this.#database[table].push(data)
     } else {
         this.#database[table] = [data]
     }
+
+    this.#persist();
 
     return data;
     }
