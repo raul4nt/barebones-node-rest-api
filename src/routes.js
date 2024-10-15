29e6d9b086ded8d12a8826a7dc8c23c31da7db97
Raul Silveira <raulantsilveira@gmail.com>
2024-10-15 19:36:41 -0300

    put function implemented

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,6 @@ export const routes = [
         path: buildRoutePath('/users'),
         handler: (req, res) => {
             const users = database.select('users')
-
             return res.end(JSON.stringify(users))
         }
     },
@@ -27,20 +26,31 @@ export const routes = [
             }
 
             database.insert('users', user)
-
             return res.writeHead(201).end()
         }
     },
     {
-     method: 'DELETE',
-     path: buildRoutePath('/users/:id'),
-     handler: (req, res) => {
-        
-        const { id } = req.params
-        
-        database.delete("users", id)
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.delete("users", id)
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { name, email } = req.body
 
-        return res.writeHead(204).end()
-      },   
+            database.update("users", id, {
+                name,
+                email,
+            })
+
+            return res.writeHead(204).end()
+        }
     }
 ]
